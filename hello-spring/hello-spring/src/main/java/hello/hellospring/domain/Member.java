b088e92a42ea82f30c4b86ed1924f48df90da4b1
신관규 <soundbar91@gmail.com>
2024-01-19 18:55:46 +0900

    JPA

--- hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,18 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+// JPA가 관리하는 엔티티
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // DB가 알아서 생성
     private Long id;
+
     private String name;
 
     public Long getId() {
